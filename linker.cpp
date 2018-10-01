67c9602016ebaff81ef83201ec7d65ee8715e013
zzzyhtheonly <zyhtheonly@yeah.net>
2018-10-01 02:52:20 -0400

    rebuild the parsing mode

--- linker.cpp
+++ linker.cpp
@@ -12,24 +12,23 @@
 
 using namespace std;
 
-int modCount = 0;
 int line = 1;
 int linecount = -1;
-
-string text;
 int idx = 0;
 
+// Metadata in each modules
 class module
 {
 public:
-    map<string,int> symVal;
-	int countVal;
-	
+    int moduleNO;
+
+	// Uselist
 	vector<string> symName;
 	map<string,int> uselist;
+
+	// Counts
+	int countVal;
 	int countName;
-	
-	vector<string> ins;
 	int countIns;
 
 	int offset;
@@ -41,9 +40,11 @@ public:
 module *modules = new module();
 module *head = modules;
 
-map<string,int> allVal;
+// Symbol Table
+map<string,int> allVal;       
 map<string,int> ValMod;
 
+// Error Type
 static string errstr[] = 
 {
     "NUM_EXPECTED",           // Number expect
@@ -60,11 +61,8 @@ enum errors
     NUM_EXPECTED, SYM_EXPECTED, ADDR_EXPECTED, SYM_TOO_LONG, TOO_MANY_DEF_IN_MODULE, TOO_MANY_USE_IN_MODULE, TOO_MANY_INSTR
 };
 
-map<string,int> ValErr;
-map<string,int> ValUse;
-map<string,int> ValDef;
-vector<string> InsErr;
 int totalIns = 0;
+int countIns = 0;
 
 string readFileIntoString(const char * filename)
 {
@@ -82,7 +80,7 @@ static inline void syntaxError(int e,int errline,int erridx,int errlinecount)
 	exit(0);
 }
 
-static inline void filter()
+static inline void filter(string text)
 {
     for(;idx < text.size() - 1 && text[idx + 1] != EOF && (text[idx] == ' ' || text[idx] == '\n' || text[idx] == '\t');idx++)
 	{
@@ -94,7 +92,7 @@ static inline void filter()
 	}
 }
 
-void ParseVal1()
+void ReadSym(string text,map<string,int>&symVal)
 {
     string tmp;
 	int count;
@@ -121,7 +119,7 @@ void ParseVal1()
 
 	for(;count != 0;count--)
 	{
-		filter();
+		filter(text);
 		errline = line;
 	    erridx = idx;
 	    errlinecount = linecount;
@@ -142,7 +140,7 @@ void ParseVal1()
 		flag = 0;
 		tmp.clear();
 
-		filter();
+		filter(text);
 		errline = line;
 	    erridx = idx;
 	    errlinecount = linecount;
@@ -156,11 +154,11 @@ void ParseVal1()
 		val = atoi(tmp.c_str());
 		tmp.clear();
 
-		modules->symVal[valname] = val + modules->offset;
+		symVal[valname] = val + modules->offset;
 	}
 }
 
-void ParseTable1()
+void StoreUselist(string text)
 {
     string tmp;
 	int count;
@@ -177,14 +175,15 @@ void ParseTable1()
 	if(tmp.size() == 0)
 		syntaxError(NUM_EXPECTED,errline,erridx,errlinecount);
 
-    count = atoi(tmp.c_str());
+	modules->countName = atoi(tmp.c_str());
+	count = modules->countName;
 	if(count > 16)
 		syntaxError(TOO_MANY_USE_IN_MODULE,errline,erridx,errlinecount);
 	tmp.clear();
 
 	for(;count != 0;count--)
 	{
-	    filter();
+	    filter(text);
 		errline = line;
 	    erridx = idx;
 	    errlinecount = linecount;
@@ -202,12 +201,14 @@ void ParseTable1()
 			syntaxError(SYM_TOO_LONG,errline,erridx,errlinecount);
 
 		flag = 0;
+		modules->symName.push_back(tmp);	
+		modules->uselist[tmp] = 0;
 		tmp.clear();
 	}
 	
 }
 
-void ParseIns1()
+void DetectIns(string text)
 {
     string tmp;
 	int count;
@@ -231,7 +232,7 @@ void ParseIns1()
 
 	for(;count != 0;count--)
 	{
-	    filter();
+	    filter(text);
 	    errline = line;
 	    erridx = idx;
 	    errlinecount = linecount;
@@ -241,7 +242,7 @@ void ParseIns1()
 		else
 			syntaxError(ADDR_EXPECTED,errline,erridx,errlinecount);
 
-		filter();
+		filter(text);
 		errline = line;
 	    erridx = idx;
 	    errlinecount = linecount;
@@ -253,9 +254,34 @@ void ParseIns1()
 		    syntaxError(NUM_EXPECTED,errline,erridx,errlinecount);
 		tmp.clear();
 	}
+
 }
 
-void ParseVal2()
+void BuildSymtable(map<string,int> symVal,map<string,int>&ValErr)
+{
+    map<string,int>:: iterator iter;
+	for(iter = symVal.begin(); iter != symVal.end();iter++)
+	{
+		if(allVal.find(iter->first) != allVal.end())
+		{
+			ValErr[iter->first] = 1;
+			continue;
+		}
+		else
+		{
+			ValMod[iter->first] = modules->moduleNO;
+			allVal[iter->first] = iter->second;
+		}
+
+		if(iter->second > modules->offset + modules->countIns - 1)
+		{
+			cout <<"Warning: Module " << modules->moduleNO << ": "<< iter->first << " too big " << iter->second << " (max=" << (modules->offset + modules->countIns - 1) <<") assume zero relative" <<endl;
+			allVal[iter->first] = 0;
+		}
+	}
+}
+
+void PassUseless(string text)
 {
 	int count = modules->countVal;
 	
@@ -263,147 +289,176 @@ void ParseVal2()
 
 	for(;count != 0;count--)
 	{
-		filter();	
+		filter(text);	
 	    for(;text[idx] != ' ' && text[idx] != '\n' && text[idx] != '\t';idx++);
-		filter();
+		filter(text);
 		for(;text[idx] >= '0' && text[idx] <= '9';idx++);
 	}
-}
-
-void ParseTable2()
-{
-    string tmp;
-	int count;
-
-	for(;text[idx] >= '0' && text[idx] <= '9';idx++)
-        tmp += text[idx]; 
-
-    modules->countName = atoi(tmp.c_str());
+    filter(text);
 	count = modules->countName;
-	tmp.clear();
+	
+	for(;text[idx] >= '0' && text[idx] <= '9';idx++);
 
 	for(;count != 0;count--)
 	{
-	    filter();
-		
-	    for(;text[idx] != ' ' && text[idx] != '\n' && text[idx] != '\t';idx++)
-	        tmp += text[idx];
-
-		modules->symName.push_back(tmp);	
-		modules->uselist[tmp] = 0;
-		tmp.clear();
+	    filter(text);	
+	    for(;text[idx] != ' ' && text[idx] != '\n' && text[idx] != '\t';idx++);
 	}
-	
 }
 
-void ParseIns2()
+void GenerateInsOutput(string text,map<string,int>&ValUse)
 {
-    string tmp;
+    string ins;
+	string InsErr;
 	int count = modules->countIns;
 
 	for(;text[idx] >= '0' && text[idx] <= '9';idx++);
 
 	for(;count != 0;count--)
 	{
-	    filter();
+	    filter(text);
 		
 	    if(text[idx] == 'R' || text[idx] == 'I' || text[idx] == 'E' || text[idx] == 'A')
-			tmp += text[idx++];
+			ins += text[idx++];
 
-		filter();
+		filter(text);
 
 		for(;text[idx] >= '0' && text[idx] <= '9';idx++)
-			tmp += text[idx];
-		
-		modules->ins.push_back(tmp);
-		tmp.clear();
+			ins += text[idx];
+				
+	    string str;
+		int tmp;
+		int size = ins.size();
+		if(size > 5)
+	    {
+	        if(ins[0] == 'I')
+				InsErr = "Error: Illegal immediate value; treated as 9999";
+			else
+				InsErr = "Error: Illegal opcode; treated as 9999";
+			tmp = 9999;
+			goto out;
+		}			
+		if(ins[0] == 'I')
+		{
+			tmp = atoi(ins.substr(1,size - 1).c_str());
+			InsErr = "No";
+		}
+		else if(ins[0] == 'A')
+		{
+			tmp = atoi(ins.substr(2,4).c_str());
+			if(tmp >= 512)
+			{
+				tmp = atoi((ins.substr(1,1) + "000").c_str());
+				InsErr = "Error: Absolute address exceeds machine size; zero used";
+			}
+			else
+			{
+				tmp = atoi((ins.substr(1,4)).c_str());
+				InsErr = "No";
+			}
+		}
+		else if(ins[0] == 'R' )
+		{
+			if(size == 5)
+			    tmp = atoi(ins.substr(2,4).c_str());
+			else
+				tmp = atoi(ins.substr(1,size - 1).c_str());
+			if(tmp > modules->offset + modules->countIns - 1)
+			{
+				tmp = atoi((ins.substr(1,1)).c_str()) * 1000 + modules->offset;
+				InsErr = "Error: Relative address exceeds module size; zero used";
+			}
+			else
+			{
+				tmp = atoi((ins.substr(1,size - 1)).c_str()) + modules->offset;
+				InsErr = "No";
+			}
+		}
+		else if(ins[0] == 'E')
+		{
+			tmp = atoi((ins.substr(2,4).c_str()));
+			if(tmp >= modules->countName)
+			{
+				tmp = atoi((ins.substr(1,4)).c_str());
+				InsErr = "Error: External address exceeds length of uselist; treated as immediate";
+				goto out;
+			}
+			str = modules->symName[tmp];
+			if(str.size() == 0 || allVal.find(str) == allVal.end() )
+			{
+				modules->uselist[str]++;
+				str = "Error: " + str + " is not defined; zero used";
+				tmp = 0 + atoi((ins.substr(1,4)).c_str()) - tmp;
+				InsErr = str;
+			}
+			else
+			{
+				tmp = allVal[str] + atoi((ins.substr(1,4)).c_str()) - tmp;
+				ValUse[str]++;
+				modules->uselist[str]++;
+				InsErr = "No";
+			}
+		}
+		out:
+		str = to_string(tmp);
+		for(;str.size() < 4;)
+			str = "0" + str;
+		ins = str;
+
+        if(countIns < 10)
+	        cout<< "00" << countIns << ":" << " " << ins;
+		else if(countIns >= 10 && countIns < 100)
+			cout<< "0" << countIns << ":" << " " << ins;
+		else if(countIns >= 100)
+			cout<< countIns << ":" << " " << ins;
+	    if(InsErr != "No")
+			cout<< " " << InsErr;
+		cout<< endl;
+		countIns++;
+
+		ins.clear();
 	}
 }
 
-void ParseModule1()
+void ParseAndStoreModuleSym(string text,map<string,int>&ValErr)
 {
     if(modules->prev != NULL)
+    {
 		modules->offset = modules->prev->offset + modules->prev->countIns;
+		modules->moduleNO = modules->prev->moduleNO + 1;
+    }
 	else
-		modules->offset = 0;	
-	
-    ParseVal1();
-	filter();
-	ParseTable1();
-	filter();
-	ParseIns1();
-}
-
-void ParseModule2()
-{	
-    ParseVal2();
-	filter();
-	ParseTable2();
-	filter();
-	ParseIns2();
+	{
+		modules->offset = 0;
+		modules->moduleNO = 1;
+	}
+	map<string,int> symVal;
+    ReadSym(text,symVal);
+	filter(text);
+	StoreUselist(text);
+	filter(text);
+	DetectIns(text);
+	BuildSymtable(symVal,ValErr);
 }
 
 void Passone(string filename)
 {
-    text = readFileIntoString(filename.c_str());
-	filter();
+    string text = readFileIntoString(filename.c_str());
+	map<string,int> ValErr;
+	filter(text);
 	for(;idx < text.size() - 1 && text[idx + 1] != EOF;)
 	{
-		ParseModule1();
-        modCount++;
-		
+		ParseAndStoreModuleSym(text,ValErr);		
 		modules->next = new module();
 		modules->next->prev = modules;
 		modules = modules->next;
 
-		filter();
+		filter(text);
     }
 	modules = head;
 	idx = 0;
-}
 
-void Passtwo()
-{
-    filter();
-    for(;idx < text.size() - 1 && text[idx + 1] != EOF;)
-	{
-		ParseModule2();
-		modules = modules->next;
-		filter();
-    }
-	modules = head;
-}
-
-void OutAllVal()
-{
-	map<string,int>:: iterator iter;
-    for(int i = 0;i < modCount;i++)
-    {
-		for(iter = modules->symVal.begin(); iter != modules->symVal.end();iter++)
-		{
-		    if(allVal[iter->first] != 0)
-			{
-				ValErr[iter->first] = 1;
-				continue;
-		    }
-			else
-			{
-			    ValMod[iter->first] = i + 1;
-				ValDef[iter->first] = 1;
-			    allVal[iter->first] = iter->second;
-			}
-
-			if(iter->second > modules->offset + modules->countIns - 1)
-			{
-			    cout <<"Warning: Module " << (i + 1) << ": "<< iter->first << " too big " << iter->second << " (max=" << (modules->offset + modules->countIns - 1) <<") assume zero relative" <<endl;
-				allVal[iter->first] = 0;
-			}
-		}
-
-		modules = modules->next;
-    }
-	modules = head;
-
+    map<string,int> :: iterator iter;
 	cout<< "Symbol Table" << endl;
 	for(iter = allVal.begin(); iter != allVal.end();iter++)
 	{
@@ -415,125 +470,28 @@ void OutAllVal()
 	cout<< endl;
 }
 
-void OutAllIns()
+void Passtwo(string filename)
 {
-    for(int i = 0;i < modCount;i++)
-	{
-	    for(int j = 0;j < modules->countIns;j++)
-	    {
-	        string str;
-			int tmp;
-			int size = modules->ins[j].size();
-	        if(size > 5)
-	        {
-	            if(modules->ins[j][0] == 'I')
-	                InsErr.push_back("Error: Illegal immediate value; treated as 9999");
-				else
-				    InsErr.push_back("Error: Illegal opcode; treated as 9999");
-				tmp = 9999;
-				goto out;
-	        }
-			
-	        if(modules->ins[j][0] == 'I')
-			{
-			    tmp = atoi(modules->ins[j].substr(1,size - 1).c_str());
-				InsErr.push_back("No");
-	        }
-			else if(modules->ins[j][0] == 'A')
-			{
-			    tmp = atoi(modules->ins[j].substr(2,4).c_str());
-				if(tmp >= 512)
-				{
-				    tmp = atoi((modules->ins[j].substr(1,1) + "000").c_str());
-				    InsErr.push_back("Error: Absolute address exceeds machine size; zero used");
-				}
-				else
-				{
-				    tmp = atoi((modules->ins[j].substr(1,4)).c_str());
-				    InsErr.push_back("No");
-				}
-			}
-			else if(modules->ins[j][0] == 'R' )
-			{
-			    if(size == 5)
-					tmp = atoi(modules->ins[j].substr(2,4).c_str());
-				else
-					tmp = atoi(modules->ins[j].substr(1,size - 1).c_str());
-				if(tmp > modules->offset + modules->countIns - 1)
-				{
-				    tmp = atoi((modules->ins[j].substr(1,1)).c_str()) * 1000 + modules->offset;
-				    InsErr.push_back("Error: Relative address exceeds module size; zero used");
-				}
-				else
-				{
-				    tmp = atoi((modules->ins[j].substr(1,size - 1)).c_str()) + modules->offset;
-					InsErr.push_back("No");
-				}
-			}
-			else if(modules->ins[j][0] == 'E')
-			{
-			    tmp = atoi((modules->ins[j].substr(2,4).c_str()));
-				if(tmp >= modules->countName)
-				{
-				    tmp = atoi((modules->ins[j].substr(1,4)).c_str());
-				    InsErr.push_back("Error: External address exceeds length of uselist; treated as immediate");
-					goto out;
-				}
-				str = modules->symName[tmp];
-				if(str.size() == 0 || ValDef[str] != 1)
-				{
-				    modules->uselist[str]++;
-				    str = "Error: " + str + " is not defined; zero used";
-				    tmp = 0 + atoi((modules->ins[j].substr(1,4)).c_str()) - tmp;
-				    InsErr.push_back(str);
-				}
-				else
-				{
-				    tmp = allVal[str] + atoi((modules->ins[j].substr(1,4)).c_str()) - tmp;
-					ValUse[str]++;
-					modules->uselist[str]++;
-					InsErr.push_back("No");
-				}
-			}
-			out:
-			str = to_string(tmp);
-			for(;str.size() < 4;)
-			    str = "0" + str;
-			modules->ins[j] = str;
-	    }
-		
-		modules = modules->next;
-	}
-
-	modules = head;
-
-	map<string,int>:: iterator iter;
+    string text = readFileIntoString(filename.c_str());
+    map<string,int>:: iterator iter;
+	map<string,int> ValUse;
 	cout<< "Memory Map" << endl;
-    int countIns = 0;
-	for(int i = 0;i < modCount;i++)
+    filter(text);
+    for(;idx < text.size() - 1 && text[idx + 1] != EOF;)
 	{
-	    for(int j = 0;j < modules->countIns;j++,countIns++)
+	    map<string,int> use;
+		PassUseless(text);
+	    filter(text);
+	    GenerateInsOutput(text,ValUse);
+	    for(iter = modules->uselist.begin();iter != modules->uselist.end();iter++)
 	    {
-	        if(countIns < 10)
-	            cout<< "00" << countIns << ":" << " " << modules->ins[j];
-		    else if(countIns >= 10 && countIns < 100)
-			    cout<< "0" << countIns << ":" << " " << modules->ins[j];
-		    else if(countIns >= 100)
-			    cout<< countIns << ":" << " " << modules->ins[j];
-			if(InsErr[countIns] != "No")
-			    cout<< " " << InsErr[countIns];
-		    cout<< endl;
-	    }
-		for(iter = modules->uselist.begin();iter != modules->uselist.end();iter++)
-		{
 		    if(iter->second == 0)
-				cout<<"Warning: Module " << (i + 1) << ": " << iter->first << " appeared in the uselist but was not actually used" << endl;
-		}
+		        cout<<"Warning: Module " << modules->moduleNO << ": " << iter->first << " appeared in the uselist but was not actually used" << endl;
+	    }
 		modules = modules->next;
-
-	}
+		filter(text);
+    }
 	modules = head;
-	cout<< endl;
 
 	for(iter = allVal.begin(); iter != allVal.end();iter++)
 	{
@@ -548,11 +506,8 @@ void OutAllIns()
 int main(int argc,char* argv[])
 {
     string fn = argv[1];
-
 	Passone(fn);
-	OutAllVal();
-	Passtwo();
-	OutAllIns();
+	Passtwo(fn);
     return 0;
 }
 
